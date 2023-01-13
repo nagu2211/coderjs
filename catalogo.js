7b083bc514a07a57714c3c8704ff9561e9ec1d01
Santiago Espindola <santiago2211ar@hotmail.com>
2023-01-13 15:08:23 -0300

    compresion de imagenes

--- catalogo.js
+++ catalogo.js
@@ -1,17 +1,17 @@
 /* PRODUCTOS DEL CATALOGO */ 
 let stockProductos = [
-    {id: 1, nombre: "Qwerty Air Max", tipo:"sport", cantidad: 1, desc: "Zapatillas aerodinamicas semi deslizante especialmente para deportistas ", precio: 25000, img:'../images/prueba-card.jpg'},
-    {id: 2, nombre: "Qwerty All Stars", tipo:"sport", cantidad: 1, desc: "Zapatillas aerodinamicas semi deslizante especialmente para deportistas ", precio: 25000, img:'../images/prueba-card2.jpg'},
-    {id: 3, nombre: "New Balance", tipo:"sport", cantidad: 1, desc: "Zapatillas aerodinamicas semi deslizante especialmente para deportistas ", precio: 20000, img:'../images/prueba-card3.jpg'},
-    {id: 4, nombre: "Air Jordan", tipo:"sport", cantidad: 1, desc: "Zapatillas aerodinamicas semi deslizante especialmente para deportistas ", precio: 15000, img:'../images/prueba-card4.jpg'},
-    {id: 5, nombre: "Qwerty Gazelle", tipo:"sport", cantidad: 1, desc: "Zapatillas aerodinamicas semi deslizante especialmente para deportistas ", precio: 30000, img:'../images/prueba-card5.jpg'},
-    {id: 6, nombre: "Qwerty Classic", tipo:"sport", cantidad: 1, desc: "Zapatillas aerodinamicas semi deslizante especialmente para deportistas ", precio: 15000, img:'../images/prueba-card6.jpg'},
-    {id: 7, nombre: "Wedgie Booties", tipo:"party", cantidad: 1, desc: "Botas que sobresalen en medio de la multitud especialmente para extrovertidos ", precio: 15000, img:'../images/cat7.jpg'},
-    {id: 8, nombre: "Chelsea Boots", tipo:"casual/elegant", cantidad: 1, desc: "Botas elegantes o tipo casual , para tu reunion laboral o incluso para un paseo", precio: 22000, img:'../images/cat8.jpg'},
-    {id: 9, nombre: "Nike Crazy Red", tipo:"sport/casual", cantidad: 1, desc: "Zapatillas de un tono rojo fuerte para destacar en tu outfit solo o acompañado.", precio: 33000, img:'../images/cat9.jpg'},
-    {id: 10, nombre: "Nike Road", tipo:"casual", cantidad: 1, desc: "Zapatillas perfectas para todos los dias o para dar un paseo, Incluso llevarlas al cine de vez en cuando", precio: 18000, img:'../images/cat10.jpg'},
-    {id: 11, nombre: "Gianvito Rossi", tipo:"elegant", cantidad: 1, desc: "Zapatos elegantes para reuniones, bautismos, casamientos. Si crees que son para ti dale a comprar", precio: 30000, img:'../images/cat11.jpg'},
-    {id: 12, nombre: "Passion Beige", tipo:"casual", cantidad: 1, desc: "Si te gustan los colores suaves y vestir bien estas zapatillas casuales de vans son perfectas para ti", precio: 20000, img:'../images/cat12.jpg'},
+    {id: 1, nombre: "Qwerty Air Max", tipo:"sport", cantidad: 1, desc: "Zapatillas aerodinamicas semi deslizante especialmente para deportistas ", precio: 25000, img:'../images/prueba-card.webp'},
+    {id: 2, nombre: "Qwerty All Stars", tipo:"sport", cantidad: 1, desc: "Zapatillas aerodinamicas semi deslizante especialmente para deportistas ", precio: 25000, img:'../images/prueba-card2.webp'},
+    {id: 3, nombre: "New Balance", tipo:"sport", cantidad: 1, desc: "Zapatillas aerodinamicas semi deslizante especialmente para deportistas ", precio: 20000, img:'../images/prueba-card3.webp'},
+    {id: 4, nombre: "Air Jordan", tipo:"sport", cantidad: 1, desc: "Zapatillas aerodinamicas semi deslizante especialmente para deportistas ", precio: 15000, img:'../images/prueba-card4.webp'},
+    {id: 5, nombre: "Qwerty Gazelle", tipo:"sport", cantidad: 1, desc: "Zapatillas aerodinamicas semi deslizante especialmente para deportistas ", precio: 30000, img:'../images/prueba-card5.webp'},
+    {id: 6, nombre: "Qwerty Classic", tipo:"sport", cantidad: 1, desc: "Zapatillas aerodinamicas semi deslizante especialmente para deportistas ", precio: 15000, img:'../images/prueba-card6.webp'},
+    {id: 7, nombre: "Wedgie Booties", tipo:"party", cantidad: 1, desc: "Botas que sobresalen en medio de la multitud especialmente para extrovertidos ", precio: 15000, img:'../images/cat7.webp'},
+    {id: 8, nombre: "Chelsea Boots", tipo:"casual/elegant", cantidad: 1, desc: "Botas elegantes o tipo casual , para tu reunion laboral o incluso para un paseo", precio: 22000, img:'../images/cat8.webp'},
+    {id: 9, nombre: "Nike Crazy Red", tipo:"sport/casual", cantidad: 1, desc: "Zapatillas de un tono rojo fuerte para destacar en tu outfit solo o acompañado.", precio: 33000, img:'../images/cat9.webp'},
+    {id: 10, nombre: "Nike Road", tipo:"casual", cantidad: 1, desc: "Zapatillas perfectas para todos los dias o para dar un paseo, Incluso llevarlas al cine de vez en cuando", precio: 18000, img:'../images/cat10.webp'},
+    {id: 11, nombre: "Gianvito Rossi", tipo:"elegant", cantidad: 1, desc: "Zapatos elegantes para reuniones, bautismos, casamientos. Si crees que son para ti dale a comprar", precio: 30000, img:'../images/cat11.webp'},
+    {id: 12, nombre: "Passion Beige", tipo:"casual", cantidad: 1, desc: "Si te gustan los colores suaves y vestir bien estas zapatillas casuales de vans son perfectas para ti", precio: 20000, img:'../images/cat12.webp'},
 ]
 /* VARIABLES */
 const contenedor = document.getElementById('contenedor')
